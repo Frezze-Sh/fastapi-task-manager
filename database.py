aa9fc3ee1148b7e8ee4b51806df582f1d558887d
Оразлиев Шамиль <orazlievshamil@gmail.com>
2026-06-11 18:39:21 +0300

    small fix

--- database.py
+++ database.py
@@ -18,24 +18,3 @@ def get_cursor():
     """Возвращает курсор с RealDictCursor (результаты в виде словарей)"""
     conn = get_connection()
     return conn, conn.cursor(cursor_factory=RealDictCursor)
-
-
-# import psycopg2
-# from psycopg2.extras import RealDictCursor
-# DB_CONFIG = {
-#     "host": "127.0.0.1",
-#     "port": 5433,
-#     "database": "taskdb",
-#     "user": "postgres",
-#     "password": "mysecretpassword"
-# }
-#
-# def get_connection():
-#     """Возвращает соединение с базой данных"""
-#     return psycopg2.connect(**DB_CONFIG)
-#
-#
-# def get_cursor():
-#     """Возвращает курсор с RealDictCursor (результаты в виде словарей)"""
-#     conn = get_connection()
-#     return conn, conn.cursor(cursor_factory=RealDictCursor)
